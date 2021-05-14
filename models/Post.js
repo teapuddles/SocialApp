825a3373250e8fd588080de74e641ddc5d2429e0
teapuddles <kingsley.gleeson@gmail.com>
2021-05-14 15:10:36 -0400

    created Posts model

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,32 @@
 const { model, Schema } = requires('mongoose');
+
+const postSchema = new Schema({
+     body: String,
+     username: String,
+     createdAt: String,
+    //  each post has an ARRAY OF COMMENTS
+    // AND AN ARRAY OF LIKES
+    // define each part of these attributes here. 
+    // this is really neat. Like customizing a serializer.
+     comments: [
+         {
+             body: String,
+             username: String,
+             createdAt: String
+         }
+        ],
+     likes: [
+         {
+            username: String,
+            createdAt: String
+         }
+        ],
+        // mongo db doesn't have relations between models,
+        // the ORM lets us create them though!
+        // I belive that this is sort of like self referential stuff
+        // in ruby? Lets see how this plays out.
+     user: {
+            type: Schema.Types.ObjectId,
+            ref: 'users'
+        }
+})
