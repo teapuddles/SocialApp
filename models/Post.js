2d3f995b04609914c6b9c18256eddfa5696045a5
teapuddles <kingsley.gleeson@gmail.com>
2021-05-14 15:02:22 -0400

    created User model

---
+++ models/Post.js
@@ -0,0 +1,1 @@
+const { model, Schema } = requires('mongoose');
---
+++ models/User.js
@@ -0,0 +1,13 @@
+// just like in ruby, this is how you'll set up you're schema
+const { model, Schema } = requires('mongoose');
+
+const userSchema = new Schema({
+    // we could specifiy if our attributes are 
+    // required, but we'll do it in gql instead!
+    username: String,
+    password: String, 
+    email: String,
+    createdAt: String
+}); 
+
+module.exports = model('User', userSchema)
